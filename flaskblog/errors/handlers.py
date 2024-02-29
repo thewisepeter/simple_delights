8b7d05674e9f13f4c664702139251b35c868b477
thewisepeter <rempeterwisdom@gmail.com>
2024-02-29 10:40:40 +0300

    added error handling functionality

--- flaskblog/errors/handlers.py
+++ flaskblog/errors/handlers.py
@@ -1,18 +1,30 @@
 from flask import Blueprint, render_template
+'''
+    module that handles errors
+'''
 
 errors = Blueprint('errors', __name__)
 
 
 @errors.app_errorhandler(404)
 def error_404(error):
+    '''
+        handles 404 error
+    '''
     return render_template('errors/404.html'), 404
 
 
 @errors.app_errorhandler(403)
 def error_403(error):
+    '''
+        handles 403 error
+    '''
     return render_template('errors/403.html'), 403
 
 
 @errors.app_errorhandler(500)
 def error_500(error):
+    '''
+        handles 500 error
+    '''
     return render_template('errors/500.html'), 500
